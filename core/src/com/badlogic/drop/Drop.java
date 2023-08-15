2f669f8b711f0a46f6954174f4452c10808e7f22
miguel <upsk11317003@iscte-iul.pt>
2023-08-15 19:01:34 +0100

    hello hello

---
+++ core/src/com/badlogic/drop/Drop.java
@@ -0,0 +1,32 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Drop extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+    public GameScreen gameScreen;
+
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // use libGDX's default Arial font
+        this.setScreen(new MainMenuScreen(this));
+        gameScreen = new GameScreen(this);
+    }
+
+    public void render() {
+        super.render(); // important!
+    }
+
+   @Override
+    public void dispose() {
+       if(gameScreen != null)
+           gameScreen.dispose();
+
+       batch.dispose();
+       font.dispose();
+
+   }
+}
